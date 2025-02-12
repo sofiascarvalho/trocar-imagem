b0e77b627c338a386dcef21bd36e86180d9adcd0
sofiasc <163853687+sofiascarvalho@users.noreply.github.com>
2025-02-12 19:43:51 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,10 @@
 const botaoTrocarImagem = document.getElementById('trocar-imagem');
 
 const imagens = [
-    'url("./img/B9726803337Z.1_20210421145140_000+GEVHUSUSU.1-0.jpg")',
-    'url("./img/GettyImages-176560285-1.webp")',
-    'url("./img/Gilmore-Girls-Modo_Meu.jpg")',
-    'url("./img/julie-and-the-phantoms-segunda-temporada.jpg")'
+    'url("B9726803337Z.1_20210421145140_000+GEVHUSUSU.1-0.jpg")',
+    'url("GettyImages-176560285-1.webp")',
+    'url("Gilmore-Girls-Modo_Meu.jpg")',
+    'url("julie-and-the-phantoms-segunda-temporada.jpg")'
 ];
 
 let indiceAtual = 0;
